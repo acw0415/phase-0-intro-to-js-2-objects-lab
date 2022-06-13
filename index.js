3a561b2ef5aa86b5272ab21875332d5d61772bd7
Austin <acw0415@gmail.com>
2022-06-13 16:46:25 -0400

    commit tree 6075562161eccbf712f85072d3ee554e57bb672b

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "Richard Noggin",
+    streetAddress: "42 Wallaby Way, Sydney"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const copyEmployee = { ...employee }
+        copyEmployee[key] = value
+    return copyEmployee
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+   employee[key]= value
+   return employee
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const copyEmployee = { ...employee }
+        delete copyEmployee[key]
+    return copyEmployee 
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
